122964d7f6604781f1b4b8450d3d9de9d78caac2
Fabrizio Sciscenti <fabrizio.sciscenti@polito.it>
2025-03-12 17:36:09 +0100

    simple_workflow added

--- lib.py
+++ lib.py
@@ -20,20 +20,19 @@ def initialize_filter_coefficients(fsamp):
         b[key] = torch.tensor(b[key], dtype=torch.float32, device=torch.device("cuda" if torch.cuda.is_available() else "cpu"))
     for key in a:
         a[key] = torch.tensor(a[key], dtype=torch.float32, device=torch.device("cuda" if torch.cuda.is_available() else "cpu"))
-    print("Filter coefficients initialized")
     return b, a
 
 def filter_data(data, b, a):
-    data = data.cpu().numpy()
+    # data = data.cpu().numpy()
     filtered_data = data
     for key in b:
         filtered_data = lfilter(b[key].cpu().numpy(), a[key].cpu().numpy(), filtered_data)
     filtered_data = torch.tensor(filtered_data, dtype=torch.float32, device=torch.device("cuda" if torch.cuda.is_available() else "cpu"))
     return filtered_data
 
-def remove_artifact_50overlap(data, fsamp):
+def remove_artifact(data, fsamp):
     L = data.shape[0]
-    params = {'w': int(100e-3 * fsamp), 'threshold': 1.33}
+    params = {'w': int(100e-3 * fsamp), 'threshold': 1.44}
     artMask = torch.zeros(L, dtype=torch.bool, device=torch.device("cuda" if torch.cuda.is_available() else "cpu"))
     Wbis = 0
     t = 0
@@ -70,14 +69,17 @@ def remove_artifact_50overlap(data, fsamp):
             stop = L
             w = stop - start
 
-    artMask[-1] = True
     clearSignal = data[~artMask]
-    return clearSignal
+    return clearSignal, artMask
 
-def extract_segment_features_min_max(data, fs, max_vals, min_vals):
+def extract_segment_features_min_max(data, fs):
     segment_length = fs
     # num_segments = data.shape[0] // segment_length
     all_features = []
+
+    # normalization intervals computed from the training set
+    max_vals = [8.707813,0.016264,0.073682,0.133387,0.16357,2769,0.435164,0.936993,2.680869]
+    min_vals = [1.357211,0.002564,0.019996,-0.250695,0.041139,1644.88,0.153334,0.833393,-0.096321]
 
     overlap = 0.5
     step = int(segment_length * (1 - overlap))
@@ -115,3 +117,40 @@ def extract_segment_features_min_max(data, fs, max_vals, min_vals):
         all_features[:, i] = (all_features[:, i] - min_vals[i]) / (max_vals[i] - min_vals[i])
     all_features = np.clip(all_features, 0, 1)
     return all_features
+
+def plot_signals(patient, side, data, meta, fsamp):
+    import matplotlib.pyplot as plt
+
+    # Filter the metadata for the given patient and side
+    patient_meta = meta[(meta['patient'] == patient) & (meta['side'] == side)]
+    
+    # Sort by depth
+    patient_meta = patient_meta.sort_values(by='depth', ascending=True)
+    
+    # Extract the signals
+    signals = []
+    for idx, row in patient_meta.iterrows():
+        signal = data[idx, :row['length']]
+        signals.append(signal)
+    
+    # Normalize signals with respect to the median standard deviation
+    all_signals = np.concatenate(signals)
+    median_std = np.median(np.std(all_signals))
+    normalized_signals = [signal / (15*median_std) for signal in signals]
+
+    # Define a time vector for plotting
+    time_vector = np.arange(len(normalized_signals[0])) / fsamp
+    
+    # Plot the signals
+    fig, ax = plt.subplots(figsize=(15, 10))
+    for i, signal in enumerate(normalized_signals):
+        c = '#212e47'
+        if patient_meta.iloc[i]['class'] == 1: c = '#e26d0e'
+        ax.plot(time_vector, signal - i, linewidth=0.3, color=c)  # Offset each signal for clarity
+    
+    ax.set_yticks(-np.arange(len(normalized_signals)))
+    ax.set_yticklabels(patient_meta['depth']/1000)  # Convert depth to mm
+    ax.set_xlabel('Time (s)')
+    ax.set_ylabel('Estimated Distance from Target (mm)')
+    ax.set_title(f'Signals for {patient} - {side}')
+    plt.show()
--- testing.py
+++ testing.py
@@ -23,20 +23,39 @@ else:
 def load_data(filepath, metapath):
     with np.load(filepath) as npfh:
         data = npfh['data']
-        meta = pd.read_feather(metapath)
+        meta = pd.read_csv(metapath, sep=';')
         lens = meta['length'].to_numpy()
         clss = meta['class'].to_numpy()
 
     print(f"Data loaded with shape: {data.shape}")
     return torch.tensor(data, dtype=torch.float32).to(device), clss, lens, meta
 
-filepath = "C:/Users/fabri/Desktop/scripts_final/dataset/raw/ciecierski/raw_test.npz"
-metapath = "C:/Users/fabri/Desktop/scripts_final/dataset/raw/ciecierski/raw_test.feather"
+filepath = "C:/Users/fabri/Desktop/scripts_final/dataset/raw/UniTO-MERs/myDataset_raw_resampled.npz"
+metapath = "C:/Users/fabri/Desktop/scripts_final/dataset/raw/UniTO-MERs/metadata_myDataset_resampled.csv"
+# filepath = 'data.npz'
+# metapath = 'metadata.csv'
 print('Loading data...')
 raw_data, clss, lens, meta = load_data(filepath, metapath)
 
 # Display the first 3 rows of the table meta
 print(meta.head(3))
+
+# # Extract subset of data for patient P07, RIGHT hemisphere, first 10 seconds
+# patient_id = 'P07'
+# hemisphere = 'RIGHT'
+# subset_duration = 10 * 24000  # first 10 seconds
+# # Filter meta data for the specific patient and hemisphere
+# subset_meta = meta[(meta['patient'] == patient_id) & (meta['side'] == hemisphere)]
+# # Extract corresponding rows from raw_data
+# subset_indices = subset_meta.index.to_numpy()
+# subset_raw_data = raw_data[subset_indices, :subset_duration]
+# # Save the subset data and meta
+# subset_filepath = "subset_raw_data.npz"
+# subset_metapath = "subset_metadata.csv"
+# np.savez(subset_filepath, data=subset_raw_data.cpu().numpy())
+# subset_meta.to_csv(subset_metapath, sep=';', index=False)
+# print(f"Subset data saved to {subset_filepath} and {subset_metapath}")
+
 
 # Plot the first n signals of raw_data
 fsamp = 24000 # Hz
@@ -51,36 +70,30 @@ plt.show()
 from trained_model.MLP_architecture import MLP_STIM
 
 def load_model(model_path):
-    norm_intervals = np.loadtxt(os.path.join(model_path, 'normalization_intervals.txt'),skiprows=1, comments=None)
     model = MLP_STIM(9, 1).to(device)
     mapped_state_dict = torch.load(os.path.join(model_path,'MLP_parameters.pth'), weights_only=True, map_location=device)
     model.load_state_dict(mapped_state_dict)
     model.eval()
     print(f"Model loaded from {model_path}")
-    return model, norm_intervals
+    return model
 
 model_path = os.path.join(os.getcwd(),'trained_model')
 fsamp = 24000
 b, a = lib.initialize_filter_coefficients(fsamp)
 
-model, norm_intervals = load_model(model_path)
-
-max_vals_np = norm_intervals[:,1]
-min_vals_np = norm_intervals[:,0]
-max_vals = torch.tensor(max_vals_np, dtype=torch.float32, device=device)
-min_vals = torch.tensor(min_vals_np, dtype=torch.float32, device=device)
+model = load_model(model_path)
 
 #%% Main processing functions
 
-def process_recording(model, recording, fsamp, b, a, max_vals, min_vals):
+def process_recording(model, recording, fsamp, b, a):
     start_time = time.time()
     filtered_data = lib.filter_data(recording, b, a)
-    artifact_free_data = lib.remove_artifact_50overlap(filtered_data, fsamp)
+    artifact_free_data, _ = lib.remove_artifact(filtered_data, fsamp)
     if artifact_free_data.shape[0] < fsamp:
         predictions = np.array([])
         dt = time.time() - start_time
     else:
-        features = lib.extract_segment_features_min_max(artifact_free_data.cpu().numpy(), fsamp, max_vals.cpu().numpy(), min_vals.cpu().numpy())
+        features = lib.extract_segment_features_min_max(artifact_free_data.cpu().numpy(), fsamp)
         features = torch.tensor(features, dtype=torch.float32).to(device=torch.device("cuda" if torch.cuda.is_available() else "cpu"))
         with torch.no_grad():
             predictions = model(features)
@@ -89,19 +102,19 @@ def process_recording(model, recording, fsamp, b, a, max_vals, min_vals):
         predictions = predictions.cpu().numpy()
     return predictions, dt
 
-def main(model, raw_data, lens, fsamp, b, a, max_vals, min_vals):
+def main(model, raw_data, lens, fsamp, b, a):
     with tqdm_joblib(tqdm(total=raw_data.shape[0], desc="Processing Recordings")):
         batch_size = 32
         results = []
         for i in tqdm(range(0, raw_data.shape[0], batch_size), desc="Processing Batches"):
             batch_results = Parallel(n_jobs=4, timeout=3600)(
-                delayed(process_recording)(model, raw_data[j, :lens[j]], fsamp, b, a, max_vals, min_vals)
+                delayed(process_recording)(model, raw_data[j, :lens[j]], fsamp, b, a)
                 for j in range(i, min(i + batch_size, raw_data.shape[0])))
             results.extend(batch_results)
     predictions, dts = zip(*results)
     return predictions, dts
 
-predictions, dts = main(model, raw_data, lens, fsamp, b, a, max_vals, min_vals)
+predictions, dts = main(model, raw_data, lens, fsamp, b, a)
 df = pd.DataFrame({'prediction': predictions, 'dt': dts})
 filename = 'predictions.txt'
 df.to_csv(filename, sep='\t', index=False)
